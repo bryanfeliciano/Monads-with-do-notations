76c942e18f860b5673476598dceddb26803fec22
bryan <bryanfeliciano892@gmail.com>
2022-05-07 17:51:35 -0500

    initial commit,learning do-notation

---
+++ Lib.hs
@@ -0,0 +1,21 @@
+module Lib where
+
+data Grade = F | D | C | B | A deriving (Eq, Ord, Enum, Show, Read)
+
+data Degree = HS | BA | MS | PhD deriving (Eq, Ord, Enum, Show, Read)
+
+data Candidate = Candidate
+                          { candidateId :: Int
+                          , codeReview :: Grade
+                          , cultureFit :: Grade
+                          , education :: Degree } deriving Show
+
+viable :: Candidate -> Bool
+viable candidate = all (== True) tests
+    where 
+        passedCoding = codeReview candidate > B
+        passedCultureFit = cultureFit candidate > C
+        educationMin = education candidate >= MS
+        tests = [passedCoding
+                ,passedCultureFit
+                ,educationMin]
